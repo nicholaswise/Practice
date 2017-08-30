2712a9976007933562ecf5d55f1ada13b9af5a14
Nick Wise <nickcwise@gmail.com>
2017-08-29 22:52:40 -0400

    added practice problems

--- Practice/Program.cs
+++ Practice/Program.cs
@@ -48,6 +48,38 @@ namespace Practice
             Console.WriteLine(((4*6) / 11) + 14 - 4);
             Console.WriteLine(2+15/6*1-7%2);
 
+            //5
+            //N/A
+            //6
+            Console.WriteLine("Enter three numbers");
+            Console.WriteLine("First number: ");
+            int numOne = int.Parse(Console.ReadLine());
+            Console.WriteLine("Second number: ");
+            int numTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine("Third number: ");
+            int numThree = int.Parse(Console.ReadLine());
+            Console.WriteLine(numOne + " x " + numTwo + " x " + numThree + " = ");
+            Console.WriteLine("Press ENTER to multiply");
+            Console.ReadKey();
+            Console.WriteLine(numOne * numTwo * numThree);
+
+            //7
+            Console.WriteLine("Enter the first number");
+            int unoNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            int dosNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Press enter to see the math: ");
+            Console.ReadKey();
+            Console.WriteLine(unoNum + "+" + dosNum + "=" +(unoNum + dosNum));
+            Console.WriteLine(unoNum + "-" + dosNum + "=" +(unoNum - dosNum));
+            Console.WriteLine(unoNum + "x" + dosNum + "=" +(unoNum * dosNum));
+            Console.WriteLine(unoNum + "/" + dosNum + "=" + (unoNum / dosNum));
+            Console.WriteLine(unoNum + "%" + dosNum + "=" + (unoNum % dosNum));
+
+            //8
+
+
+
         }
     }
 }
